39ca27851e5add021f68df90106bfb82b9c7844a
rebecavareiro <rebecavareiro@gmail.com>
2024-04-20 00:48:08 -0300

    adicionando logs ao projeto

--- main.go
+++ main.go
@@ -2,23 +2,25 @@ package main
 
 import (
 	"log"
-	
+
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/rebecavarreiro/meu-primeiro-crud-go/src/configuration/logger"
 	"github.com/rebecavarreiro/meu-primeiro-crud-go/src/controller/routes"
 )
+
 func main() {
-	
+	logger.Info("About to start user application")
 	err := godotenv.Load()
-	if err != nil{
+	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
 	router := gin.Default()
-	
+
 	routes.InitRoutes(&router.RouterGroup)
 
-	if err:= router.Run(":8080"); err != nil{
+	if err := router.Run(":8080"); err != nil {
 		log.Fatal(err)
 	}
 }
--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -2,20 +2,29 @@ package controller
 
 import (
 	"fmt"
-	"log"
+
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rebecavarreiro/meu-primeiro-crud-go/src/configuration/logger"
 	"github.com/rebecavarreiro/meu-primeiro-crud-go/src/configuration/validation"
 	"github.com/rebecavarreiro/meu-primeiro-crud-go/src/controller/model/request"
+	"github.com/rebecavarreiro/meu-primeiro-crud-go/src/controller/model/response"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
-	log.Println("Init CreateUser controller")
+	logger.Info("Init CreateUser controller",
+		zap.String("journey", "createUser"),
+	)
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
 
-		log.Printf("Error trying to marshal object, error=%s\n", err.Error())
+		logger.Error("Error trying to validate user info", err,
+			zap.String("journey", "createUser"),
+		)
+
 		errRest := validation.ValidateUserError(err)
 
 		c.JSON(errRest.Code, errRest)
@@ -23,5 +32,16 @@ func CreateUser(c *gin.Context) {
 	}
 
 	fmt.Println(userRequest)
+	response := response.UserResponse{
+		ID:    "teste",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
 
+	logger.Info("User created successfully",
+		zap.String("journey", "createUser"),
+	)
+
+	c.JSON(http.StatusOK, response)
 }
